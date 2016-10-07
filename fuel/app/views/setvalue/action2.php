727899da2ab1d2ddc41a13085dca2a00a24dc5f4
Tstsuya Osawa <tatsuya.o@x-point-1.net>
2016-10-07 12:14:42 +0900

    deploy push-test

--- fuel/app/views/setvalue/action2.php
+++ fuel/app/views/setvalue/action2.php
@@ -1,6 +1,5 @@
 <ul class="nav nav-pills">
 	<li class='<?php echo Arr::get($subnav, "action2" ); ?>'><?php echo Html::anchor('setvalue/action2','Action2');?></li>
-
 </ul>
 <p>Action1</p>
 <?php
@@ -8,4 +7,4 @@ echo '<pre>';
 echo Fuel::VERSION . PHP_EOL;
 var_dump($GLOBALS);
 echo '</pre>';
-// 
+//
